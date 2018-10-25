0efb514f4718948afa572bfa18cdd8e2dca3ed45
Sven-Michael Stübe <stuebe2k7@gmail.com>
2018-10-25 19:55:59 +0200

    initial

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Examples.Controllers
+{
+    using System.Web.Http;
+
+    // breaks
+    //[Microsoft.Web.Http.ApiVersion("1.0")]
+    //[RoutePrefix( "api/v{version:apiVersion}/helloworld" )]
+
+    // working
+    [RoutePrefix("api/v1/helloworld")]
+    public class HelloWorldController : ApiController
+    {
+        [Route]
+        public IHttpActionResult Get([FromUri] IEnumerable<Guid> ids)
+        {
+            return Ok($"ids is null: {ids == null}");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Http.Routing;
+using Microsoft.Web.Http;
+using Microsoft.Web.Http.Routing;
+
+[assembly: Microsoft.Owin.OwinStartup( typeof( Microsoft.Examples.Startup ) )]
+
+namespace Microsoft.Examples
+{
+    using global::Owin;
+    using System.Web.Http;
+
+    public class Startup
+    {
+        public void Configuration( IAppBuilder builder )
+        {
+            var configuration = new HttpConfiguration();
+            var httpServer = new HttpServer( configuration );
+
+
+            // breaks:
+            //configuration.AddApiVersioning();
+            //var constraintResolver = new DefaultInlineConstraintResolver() { ConstraintMap = { ["apiVersion"] = typeof(ApiVersionRouteConstraint) } };
+            //configuration.MapHttpAttributeRoutes( constraintResolver );
+
+            // defaut (works)
+            configuration.MapHttpAttributeRoutes();
+            builder.UseWebApi( httpServer );
+        }
+    }
+}
